cde69d3a02b2e06a3b39e192d74e61da68ba366d
Yuncong Zhang <yczhangsjtu@gmail.com>
2024-09-13 12:50:43 +0800

    Refactor.

--- src/app.rs
+++ src/app.rs
@@ -12,79 +12,11 @@ use egui::{Color32, FontFamily, FontId, FontSelection, Key, RichText, TextEdit, 
 mod config;
 use config::Config;
 
+mod content;
+use content::Content;
+
 mod macos;
 mod windows;
-
-#[derive(Default, Clone)]
-enum Content<T: Clone, P: Clone> {
-    #[default]
-    None,
-    NewFile(T),
-    Encrypted(T, T, T, T),
-    PlainText(T, P, usize),
-    Error(T),
-    Success(T),
-}
-
-impl<T: Clone, P: Clone> Content<T, P> {
-    #[allow(unused)]
-    fn as_ref(&self) -> Content<&T, &P> {
-        match self {
-            Content::Encrypted(ref title, ref a, ref b, ref c) => {
-                Content::Encrypted(title, a, b, c)
-            }
-            Content::PlainText(ref title, ref a, index) => Content::PlainText(title, a, *index),
-            Content::Error(ref a) => Content::Error(a),
-            Content::Success(ref a) => Content::Success(a),
-            Content::None => Content::None,
-            Content::NewFile(ref title) => Content::NewFile(title),
-        }
-    }
-
-    #[allow(unused)]
-    fn get_plaintext(&mut self) -> Option<&P> {
-        match self {
-            Content::PlainText(_, ref a, _) => Some(a),
-            _ => None,
-        }
-    }
-
-    fn get_plaintext_mut(&mut self) -> Option<&mut P> {
-        match self {
-            Content::PlainText(_, ref mut a, _) => Some(a),
-            _ => None,
-        }
-    }
-
-    fn decrease_selected_index(&mut self) {
-        match self {
-            Content::PlainText(_, _, index) => {
-                *index = (*index).saturating_sub(1);
-            }
-            _ => {}
-        }
-    }
-
-    fn increase_selected_index(&mut self) {
-        match self {
-            Content::PlainText(_, _, index) => {
-                *index = (*index).saturating_add(1);
-            }
-            _ => {}
-        }
-    }
-
-    fn get_file_name(&self) -> Option<&T> {
-        match self {
-            Content::Encrypted(filename, _, _, _) => Some(filename),
-            Content::PlainText(filename, _, _) => Some(filename),
-            Content::Error(_) => None,
-            Content::Success(_) => None,
-            Content::NewFile(filename) => Some(filename),
-            Content::None => None,
-        }
-    }
-}
 
 #[derive(Default)]
 pub struct MyApp {
