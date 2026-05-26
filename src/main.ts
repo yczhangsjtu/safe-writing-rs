9a89a242cd4eb7c14378e5ff6074b4fec861b71d
Yuncong Zhang <yczhangsjtu@gmail.com>
2026-05-26 15:16:52 +0800

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { mount } from 'svelte';
-import App from './App.svelte';
-import './styles/global.css';
-import '@fontsource/material-icons';
+import { mount } from "svelte";
+import App from "./App.svelte";
+import "./styles/global.css";
+import "@fontsource/material-icons";
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
+  target: document.getElementById("app")!,
 });
 
 export default app;
